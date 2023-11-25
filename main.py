f95f6d89e8650bc2361918062e4d1dcf78150879
douglascrodrigues <douglascrodrigues>
2023-11-25 12:07:16 -0300

    testing flake8 hook

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ fila_teste_2.atualiza_fila()
 fila_teste_2.atualiza_fila()
 print(fila_teste_2.chama_cliente(10))
 print(fila_teste_2.chama_cliente(1))
-print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+print(fila_teste_2.estatistica('10/01/1993', 215, 'detail'))
 
 """
     lib mypy, ele valida o tipo da variavel ou parametro,
