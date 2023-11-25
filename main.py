a04dde8107d405eed5309cfb0e302437e8e7b99f
douglascrodrigues <douglascrodrigues>
2023-11-25 12:02:52 -0300

    testing flake8 hook

---
+++ main.py
@@ -0,0 +1,16 @@
+from fila_normal import FilaNormal
+from fila_prioritaria import FilaPrioritaria
+
+
+fila_teste_2 = FilaPrioritaria()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+print(fila_teste_2.chama_cliente(10))
+print(fila_teste_2.chama_cliente(1))
+print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+
+"""
+    lib mypy, ele valida o tipo da variavel ou parametro,
+    conforme passado nos typeshint.
+"""
